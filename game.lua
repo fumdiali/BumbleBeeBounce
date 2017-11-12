9a12f043453a16bd5864a4924530979f7da20466
Apple <apple@Apples-MacBook-Pro.local>
2017-11-12 13:57:44 +0100

    2nd commit.Add audio,images and create:scene clutter

--- game.lua
+++ game.lua
@@ -21,13 +21,25 @@ function scene:create( event )
        -- Code here runs when the scene is first created but has not yet appeared on screen
 
        local tapCount = 0
+       local runtime = 0
+       scrollSpeed = 2
 
-       local background = display.newImageRect("assets/images/blue-sky1.jpeg", 1040, 600 )
+       local background = display.newImageRect("assets/images/bluesky.jpg", 1020, 450 )
        background.x = display.contentCenterX
        background.y = display.contentCenterY
 
+       --mountainscape
+       local bg = display.newImageRect("assets/images/mountainscape.png", 660, 380 )
+       bg.x = display.contentCenterX
+       bg.y = display.contentCenterY
+
+       --audio/sound efx
+       local soundFlame = audio.loadStream( "assets/audio/flame-sound.mp3" )
+
+       local soundBeeJump = audio.loadStream( "assets/audio/jump-sound.mp3" )
+
        --player score display
-       local scoreDisplay = display.newText("Score:0",200, 30, native.systemFont, 24)
+       local scoreDisplay = display.newText("Score:0",200, 30, native.systemFont, 44)
        scoreDisplay:setFillColor(1,0,0)
 
        --create player lives indicator
@@ -42,32 +54,48 @@ function scene:create( event )
        life3.y = 30
 
        --create reward tokens
-       local rose = display.newImageRect("assets/images/pink-rose.png",60,60)
+       local rose = display.newImageRect("assets/images/rose-flower.png",60,60)
        rose.x = 600
-       rose.y = 125
+       rose.y = 120
        
-       transition.moveTo( rose, { x=0, y=125, iteration=0, time=6000, delay=7000 } )
+       transition.to( rose, { x=-190, y=120, time=7000, transition=easing.outInBack, delay=7000,iterations=2 } )
        physics.addBody(rose,"static")
 
-       local diamond = display.newImageRect("assets/images/dia1.png",40,40)
-       diamond.x = 265
+       local diamondBlue = display.newImageRect("assets/images/diamond-blue.png",40,40)
+       diamondBlue.x = 600
+       diamondBlue.y = 55
+       transition.to( diamondBlue, { x=-190, y=55, time=6000, transition=easing.outInBack, delay=2000,iterations=-1 } )
+       physics.addBody(diamondBlue,"static")
+
+       local redStar= display.newImageRect("assets/images/star-red.png",40,40)
+       redStar.x = 600
+       redStar.y = 120
+       transition.to( redStar, { x=-190, y=120, time=9000, transition=easing.outInBack, delay=14000,iterations=-1 } )
+       physics.addBody(redStar,"static")
+
+       local diamond = display.newImageRect("assets/images/diamond-purple.png",40,40)
+       diamond.x = 365
        diamond.y = 25
-       physics.addBody(diamond,"dynamic",{bounce=0.7})
+       physics.addBody(diamond,"dynamic",{bounce=0.9})
 
        --create player xter
        local bee = display.newImageRect("assets/images/beee.png",40,40)
        bee.x = 90
        bee.y = 100
-       physics.setGravity(0,2.5)
+       --physics.setGravity(0,2.5)
        physics.addBody( bee, "dynamic", { bounce=0.3 } )
 
+       --function to control bee floatation and player score(placeholder)
        local function bounceBee()
         tapCount = tapCount + 1
         scoreDisplay.text = "Score:"..tapCount
-        bee:applyLinearImpulse( 0, -0.05, bee.x, bee.y )
+        bee:applyLinearImpulse( 0, -0.09, bee.x, bee.y )
+        audio.play(soundBeeJump)
        end
-
+       --float our bee by tapping anywhere on the screen
        background:addEventListener( "tap", bounceBee )
+      
+       
 
        --add rectangle borders to sky/ground scape
        local roof = display.newRect(10,1,1040,0)
@@ -75,22 +103,48 @@ function scene:create( event )
        local bottom = display.newRect(10,298,1040,1)
        physics.addBody(bottom,"static")
 
-       --create ground scape
-       local ground = display.newImageRect("assets/images/ground.png",1050,120)
-       --beeLogo.x = display.contentCenterX
+       --create ground scape(grass image)
+       local ground = display.newImageRect("assets/images/grass.png",1050,120)
+
        ground.y = 260
        --physics.addBody( ground, "static")
 
-    
-       --create flame
        local flame = display.newImageRect("assets/images/flame.png",70,200)
-       flame.x = 600
+       flame.x = 700
        flame.y = 170
+       
        --set flame obj to horizontal axis
        flame.rotation = 79
-       transition.moveTo( flame, { x=0, y=170, iteration=0, time=6000, delay=4000 } )
-    
-   end
+       local flame2 = display.newImageRect("assets/images/flame.png",70,200)
+       flame2.x = 700
+       flame2.y = 270
+       --set flame obj to horizontal axis
+       flame2.rotation = 79
+       local flame3 = display.newImageRect("assets/images/flame.png",70,200)
+       flame3.x = 700
+       flame3.y = 70
+       --set flame obj to horizontal axis
+       flame3.rotation = 79
+        --section to handle flames loop
+        local function playFlameEfx()
+            audio.play(soundFlame)
+        end
+
+        function throwFlame()
+            transition.to( flame, { x=-190, y=170, time=6000, transition=easing.inExpo, delay=1000,iterations=-1,onStart=playFlameEfx } )
+            transition.to( flame2, { x=-190, y=270, time=6000, transition=easing.inExpo, delay=5000,iterations=-1,onStart=playFlameEfx } )
+            transition.to( flame3, { x=-190, y=70, time=6000, transition=easing.inExpo, delay=8000,iterations=-1,onStart=playFlameEfx } )
+           end--end of throw flame function
+            
+           ammo = 5
+           while( ammo > 0 )
+           do
+              throwFlame()
+              ammo = ammo - 1
+           end
+       
+       
+   end --end of create scene
     
     
    -- show()
@@ -101,12 +155,13 @@ function scene:create( event )
     
        if ( phase == "will" ) then
            -- Code here runs when the scene is still off screen (but is about to come on screen)
-           
+            
     
        elseif ( phase == "did" ) then
            -- Code here runs when the scene is entirely on screen
-          
-       end
+         
+       end   
+        
    end
     
     
--- menu.lua
+++ menu.lua
@@ -23,33 +23,45 @@ function scene:create( event )
     
     local sceneGroup = self.view
     -- Code here runs when the scene is first created but has not yet appeared on screen
-    local background = display.newImageRect( sceneGroup, "assets/images/blue-sky1.jpeg", 1040, 600 )
+    local background = display.newImageRect( sceneGroup, "assets/images/sky.jpeg", 1020, 600 )
     background.x = display.contentCenterX
     background.y = display.contentCenterY
+
+    local bg = display.newImageRect( sceneGroup, "assets/images/mountainscape.png", 960, 400 )
+    bg.x = display.contentCenterX
+    bg.y = display.contentCenterY
 
     local title = display.newImageRect( sceneGroup, "assets/images/bbb-logo.png", 520, 520 )
     title.x = display.contentCenterX
     title.y = 120
 
     --bee logo
-    local beeLogo = display.newImageRect("assets/images/bee.png",90,90)
+    local beeLogo = display.newImageRect("assets/images/bee-logo.png",90,90)
     beeLogo.x = display.contentCenterX
     beeLogo.y = 80
     
     --screen items
-    local diamondLogo = display.newImageRect("assets/images/dia1.png",60,60)
-    diamondLogo.x = 70
-    diamondLogo.y = 260
+    local diamondLogo = display.newImageRect("assets/images/diamond-purple.png",60,60)
+    diamondLogo.x = 40
+    diamondLogo.y = 120
 
     local flameLogo = display.newImageRect("assets/images/flame.png",100,410)
     flameLogo.x = 440
     flameLogo.y = 130
 
     local roseLogo = display.newImageRect("assets/images/rose-flower.png",60,60)
-    roseLogo.x = 310
-    roseLogo.y = 290
+    roseLogo.x = display.contentCenterX
+    roseLogo.y = 280
 
-    local playButton = display.newText( sceneGroup, "Play", display.contentCenterX, 220, native.systemFont, 34 )
+    local roseLogo = display.newImageRect("assets/images/rose-flower.png",60,60)
+    roseLogo.x = 330
+    roseLogo.y = 80
+
+    local roseLogo = display.newImageRect("assets/images/rose-flower.png",60,60)
+    roseLogo.x = 60
+    roseLogo.y = 50
+
+    --[[local playButton = display.newText( sceneGroup, "Play", display.contentCenterX, 220, native.systemFont, 34 )
     playButton:setFillColor( 0, 0, 1 )
  
     local highScoresButton = display.newText( sceneGroup, "Settings", display.contentCenterX, 260, native.systemFont, 34 )
@@ -57,7 +69,66 @@ function scene:create( event )
 
 
     playButton:addEventListener( "tap", gotoGame )
-    highScoresButton:addEventListener( "tap", gotoSettings )
+    highScoresButton:addEventListener( "tap", gotoSettings )--]]
+
+    -------------------------------------------------
+     --menu buttons
+    --------------------------------------------------
+    --[[local start = display.newText("Start Game",220, 150, native.systemFont, 26)
+    start:setFillColor(0,1,1)
+    local settings = display.newText("Game Settings",220, 150, native.systemFont, 26)
+    settings:setFillColor(0,1,1)--]]
+ 
+    
+ 
+    local widget = require( "widget" )
+
+    -- Create the widgets
+   start = widget.newButton(
+    {
+        label = "button",
+        --onEvent = handleButtonEvent,
+        emboss = false,
+        -- Properties for a rounded rectangle button
+        shape = "roundedRect",
+        width = 200,
+        height = 40,
+        cornerRadius = 2,
+        fillColor = { default={0,0,0,1}, over={1,0.1,0.7,0.4} },
+        strokeColor = { default={1,0.4,0,1}, over={0.8,0.8,1,1} },
+        strokeWidth = 4
+    }
+)
+
+  settings = widget.newButton(
+      {
+          label = "button",
+          --onEvent = handleButtonEvent,
+          emboss = false,
+          -- Properties for a rounded rectangle button
+          shape = "roundedRect",
+          width = 200,
+          height = 40,
+          cornerRadius = 2,
+          fillColor = { default={0,0,0,1}, over={1,0.1,0.7,0.4} },
+          strokeColor = { default={1,0.4,0,1}, over={0.8,0.8,1,1} },
+          strokeWidth = 4
+      }
+  )
+   
+  -- Center the button
+  start.x = display.contentCenterX - 110
+  start.y = display.contentCenterY + 70
+  settings.x = display.contentCenterX + 110
+  settings.y = display.contentCenterY + 70
+   
+  -- Change the button's label text
+  start:setLabel("Start Game")
+  settings:setLabel( "Game Settings" )
+
+  start:addEventListener( "tap", gotoGame )
+  settings:addEventListener( "tap", gotoSettings )
+ 
     
    end
     
